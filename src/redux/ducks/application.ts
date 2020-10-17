598d2cefedf5d00a33a81fbe090c856bf4bdb12e
taskinkemal <kemaltaskin@yahoo.com>
2020-10-17 23:23:41 +0200

    ongoing work

--- src/redux/ducks/application.ts
+++ src/redux/ducks/application.ts
@@ -1,18 +1,3 @@
-/* ******************************************************************************************************************
- *
- * GE CONFIDENTIAL
- *
- * Copyright (c) 2020 by General Electric Company. All Rights Reserved.
- *
- * NOTICE:  All information contained herein is confidential and proprietary to GE and/or its suppliers.
- * The intellectual and technical concepts contained herein may be covered by one or more U.S. and foreign patents,
- * and are protected by trade secret and/or copyright law. Dissemination or reproduction of this
- * information outside GE is strictly forbidden unless prior written permission is obtained from an authorized
- * representative of GE, or is in accordance with the terms and conditions contained in the written agreement
- * pursuant to which this information was supplied by GE.
- *
- ******************************************************************************************************************* */
-
 import { AsyncAction, Action } from '../types';
 import {
   deleteAccessToken,
